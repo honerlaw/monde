3b70d1a4b42ef28cbe9d6688ea07e65347bb97b4
Derek Honerlaw <derek@Dereks-MBP-3.lan>
2019-04-07 20:12:11 -0400

    update transcoder lambda to persist the new media structure

--- services/transcoder-lambda/service/media_info.go
+++ services/transcoder-lambda/service/media_info.go
@@ -10,6 +10,7 @@ import (
 	"services/server/core/repository"
 	"fmt"
 	"services/transcoder-lambda/model"
+	mediaModel "services/server/media/model"
 	"github.com/aws/aws-sdk-go/service/elastictranscoder"
 )
 
@@ -75,15 +76,15 @@ func RetryInsert(metadata *aws.S3RecordMetadata, mediainfo *model.MediaInfo, job
 		return
 	}
 
-	err := Insert(mediainfo)
+	err := Insert(metadata, mediainfo, job)
 
 	if err != nil {
 		log.Print("Failed to insert media and job information", err)
-		RetryInsert(metadata, mediainfo, job, retries - 1)
+		RetryInsert(metadata, mediainfo, job, retries-1)
 	}
 }
 
-func Insert(mediainfo *model.MediaInfo) (error) {
+func Insert(metadata *aws.S3RecordMetadata, mediainfo *model.MediaInfo, job *elastictranscoder.Job) (error) {
 	// check if there is a db connection, if not create one
 	err := repository.GetRepository().DB().Ping()
 
@@ -97,14 +98,47 @@ func Insert(mediainfo *model.MediaInfo) (error) {
 		log.Fatal(err)
 	}
 
-	// @todo this is generating an update for the tracks, when we should always insert...
-	/*tx.Create(mediainfo)
+	media := &mediaModel.Media{
+		UserID: metadata.UserID,
+		JobID:  *job.Id,
+	}
 
-	if tx.Error != nil {
-		tx.Rollback()
-		log.Print(tx.Error)
-		return tx.Error
-	}*/
+	err = repository.GetRepository().Insert(media)
+	if err != nil {
+		log.Println(err)
+		err = tx.Rollback()
+		if err != nil {
+			log.Fatal(err)
+		}
+		return nil
+	}
+
+	for _, med := range mediainfo.Medias {
+		for _, tra := range med.Tracks {
+			track := &mediaModel.Track{
+				MediaID:      media.ID,
+				Type:         tra.Type,
+				Duration:     tra.Duration,
+				Width:        tra.Width,
+				Height:       tra.Height,
+				Format:       tra.Format,
+				Encoded_Date: tra.Encoded_Date,
+				VideoCount:   tra.VideoCount,
+				DataSize:     tra.DataSize,
+				FileSize:     tra.FileSize,
+			}
+
+			err = repository.GetRepository().Insert(track)
+			if err != nil {
+				log.Println(err)
+				err = tx.Rollback()
+				if err != nil {
+					log.Fatal(err)
+				}
+				return nil
+			}
+		}
+	}
 
 	err = tx.Commit()
 	if err != nil {
