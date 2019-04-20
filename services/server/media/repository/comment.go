e27df808ac00b1f5978f98034ca77a03b05a86b9
Derek Honerlaw <derek@Dereks-MBP-3.lan>
2019-04-19 22:18:55 -0400

    convert found comments into a hierarchy

--- services/server/media/repository/comment.go
+++ services/server/media/repository/comment.go
@@ -3,8 +3,8 @@ package repository
 import (
 	"services/server/core/repository"
 	"services/server/media/model"
-	"github.com/Masterminds/squirrel"
 	"github.com/labstack/gommon/log"
+	"github.com/Masterminds/squirrel"
 )
 
 type CommentRepository struct {
@@ -41,15 +41,15 @@ func (repo *CommentRepository) GetByMediaID(mediaID string) ([]model.Comment, er
 		return nil, err
 	}
 
+	// build the map of id => comment
 	parsed := repo.repo.Parse(&model.Comment{}, rows)
 	comments := make([]model.Comment, len(parsed))
-	for i, comment := range parsed {
-		comments[i] = comment.(model.Comment)
+	for i, p := range parsed {
+		comments[i] = p.(model.Comment)
 	}
 
 	return comments, nil
 }
-
 
 func (repo *CommentRepository) Save(comment *model.Comment) (error) {
 	return repo.repo.Save(comment)
--- services/server/media/service/comment.go
+++ services/server/media/service/comment.go
@@ -11,6 +11,11 @@ type CommentRequest struct {
 	ParentCommentID string `form:"parent_comment_id"`
 }
 
+type CommentResponse struct {
+	model.Comment
+	Children []CommentResponse `json:"children"`
+}
+
 type CommentService struct {
 	commentRepository *repository.CommentRepository
 }
@@ -21,8 +26,50 @@ func NewCommentService(commentRepository *repository.CommentRepository) (*Commen
 	}
 }
 
-func (service *CommentService) GetByMediaID(id string) ([]model.Comment, error) {
-	return service.commentRepository.GetByMediaID(id)
+func (service *CommentService) GetByMediaID(id string) ([]CommentResponse, error) {
+	comments, err := service.commentRepository.GetByMediaID(id)
+	if err != nil {
+		return nil, err;
+	}
+
+	resultMap := make(map[string]CommentResponse)
+
+	for _, comment := range comments {
+
+		// does not exist in map, so add it
+		if _, ok := resultMap[comment.ID]; !ok {
+			resp := CommentResponse{
+				Children: make([]CommentResponse, 0),
+			}
+			resp.ID = comment.ID
+			resp.CreatedAt = comment.CreatedAt
+			resp.UpdatedAt = comment.UpdatedAt
+			resp.DeletedAt = comment.DeletedAt
+			resp.ParentCommentID = comment.ParentCommentID
+			resp.MediaID = resp.MediaID
+			resp.Comment = resp.Comment
+		}
+	}
+
+	// build the hierarchy
+	for _, pcom := range comments {
+		comment, _ := resultMap[pcom.ID]
+		if parentComment, ok := resultMap[pcom.ParentCommentID]; ok {
+			parentComment.Children = append(parentComment.Children, comment)
+
+			// this comment, became a child, so remove it from the map entirely
+			// this basically leaves only the "roots" that don't have parents in the map
+			delete(resultMap, pcom.ID)
+		}
+	}
+
+	// convert the map to an array of values
+	roots := make([]CommentResponse, len(resultMap))
+	for _, resp := range resultMap {
+		roots = append(roots, resp)
+	}
+
+	return roots, nil
 }
 
 func (service *CommentService) GetByID(id string) (*model.Comment, error) {
@@ -41,9 +88,9 @@ func (service *CommentService) Create(id string, req CommentRequest) (error) {
 	}
 
 	return service.Save(&model.Comment{
-		MediaID: id,
+		MediaID:         id,
 		ParentCommentID: *parentCommentId,
-		Comment: req.Comment,
+		Comment:         req.Comment,
 	})
 }
 
